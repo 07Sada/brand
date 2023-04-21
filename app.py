6065284c5a0120d55c97a30a7b9f9857181ce740
07Sada <sadashiv.nandanikar07@gmail.com>
2023-04-21 13:09:21 +0530

    data ingestion updated

--- app.py
+++ app.py
@@ -1,3 +1,8 @@
-from BrandRecognition.logger import logging
+from BrandRecognition.pipeline.training_pipelines import TrainPipeline 
+
+obj = TrainPipeline()
+obj.run_pipeline()
+
+print('Training pipeline finished!')
 
 
